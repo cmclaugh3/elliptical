8b3d83c5f26671967bf25ce51c7275329106c75a
Conor McLaughlin <cfm5qc@virginia.edu>
2017-06-02 15:32:00 +0200

    Added planet Makemake

--- R/planets.R
+++ R/planets.R
@@ -10,7 +10,8 @@
 #'   \item{eccentricity}{orbital eccentricity.}
 #' }
 #' @source
-#' These data were constructed using information from NASA's Planetary Fact Sheet at http://nssdc.gsfc.nasa.gov/planetary/factsheet/.
+#' The first 9 planets data were constructed using information from NASA's Planetary Fact Sheet at http://nssdc.gsfc.nasa.gov/planetary/factsheet/ and the information for Makemake was found in https://en.wikipedia.org/wiki/Makemake.
+
 #' @examples
 #' plot(eccentricity ~ distance, data = planets)
 "planets"
@@ -19,9 +20,9 @@ if (FALSE) {
   # Distances are in units of 1 000 000 km.
   #
   planets <- data.frame(
-    name = c('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'),
-    distance = c(57.9, 108.2, 149.6, 227.9, 778.6, 1433.5, 2872.5, 4495.1, 5906.4),
-    eccentricity = c(0.205, 0.007, 0.017, 0.094, 0.049, 0.057, 0.046, 0.011, 0.244)
+    name = c('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Makemake'),
+    distance = c(57.9, 108.2, 149.6, 227.9, 778.6, 1433.5, 2872.5, 4495.1, 5906.4, 7840.0),
+    eccentricity = c(0.205, 0.007, 0.017, 0.094, 0.049, 0.057, 0.046, 0.011, 0.244, 0.155)
   )
   #
   # Convert from km to AU.
